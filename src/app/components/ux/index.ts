4d8fbe81b4711c6edf0be101583130b62e30b1a8
Bach16 <gwyn56717@gmail.com>
2024-01-27 02:53:26 -0500

    Update index.ts

--- src/app/components/ux/index.ts
+++ src/app/components/ux/index.ts
@@ -1,3 +1,9 @@
+export { default as SocialContainer } from './SocialContainer';
 export { default as CallButton } from './button/CallButton';
+export { default as SocialButton } from './button/SocialButton';
+export { default as WalletButton } from './button/WalletButton';
 export { default as BenefitsCard } from './cards/BenefitsCard';
 export { default as Card } from './cards/Card';
+export { default as NFTCard } from './cards/NFTCard';
+export { default as FilterItem } from './items/FilterItem';
+export { default as InfoItem } from './items/InfoItem';
