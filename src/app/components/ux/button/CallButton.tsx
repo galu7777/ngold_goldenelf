2328c7b86b435f6055d2680080bf72bd0959f128
Bach16 <gwyn56717@gmail.com>
2024-01-27 02:53:09 -0500

    Update CallButton.tsx

--- src/app/components/ux/button/CallButton.tsx
+++ src/app/components/ux/button/CallButton.tsx
@@ -2,21 +2,17 @@ import { FC } from "react"
 
 interface Props {
     text:string
-    styles:string
+    styles?:string
     onClick?:()=>void
     icon?: React.ReactNode
   }
 
-const CallButton:FC<Props> = ({text,styles,onClick,icon}) => {
-
-    const displayLastNumbers = (str:string)=>{
-        return str.slice(0,2) + "..." + str.slice(-4)
-    }    
+const CallButton:FC<Props> = ({text,styles,onClick,icon}) => {     
 
   return (
     <button onClick={onClick} className={`${styles}  ${icon?"justify-evenly":"justify-center"} h-[3rem] sm:h-[2.5rem] flex items-center rounded-[30px]`}>
         {icon}
-        <p>{icon?displayLastNumbers(text):text}</p>
+        <p>{icon?text:text}</p>
     </button>
   )
 }
