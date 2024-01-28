8fe96586eb6718db47e20e89a3b224716d904f58
Bach16 <gwyn56717@gmail.com>
2024-01-28 03:04:16 -0500

    Update InfoItem.tsx

--- src/app/components/ux/items/InfoItem.tsx
+++ src/app/components/ux/items/InfoItem.tsx
@@ -8,8 +8,8 @@ interface Props {
 const InfoItems:FC<Props> = ({title,content}) => {
   return (
     <div className='flex text-center flex-col items-center' >
-        <p className='text-[#f2f2f2] text-[0.75rem] font-extralight'>{title}</p>
-        <p className=' font-medium text-[0.9rem] '>{content}</p>
+        <p className='text-[#f2f2f2] text-[0.7rem] sm:text-[0.75rem] font-extralight'>{title}</p>
+        <p className=' font-medium text-[0.8rem] sm:text-[0.9rem] '>{content}</p>
     </div>
   )
 }
