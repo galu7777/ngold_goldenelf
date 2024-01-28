969f9091407d4b53e93d7792e6571f1ba96480b0
Bach16 <gwyn56717@gmail.com>
2024-01-28 03:04:26 -0500

    Update Navbar.tsx

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,14 +1,23 @@
 "use client";
 import Image from "next/image";
-import { useEffect } from "react";
+import { FC, useEffect } from "react";
 import WalletButton from "./ux/button/WalletButton";
 import { HiOutlineDotsVertical } from "react-icons/hi";
 import Link from "next/link";
 import { changeNavbarColor } from "@/assets";
 
-const Navbar = () => {
+//navitems={navitems} logo={""} buttonColor={"#F6CF4E"}
+
+interface Props {
+  children?: React.ReactNode;
+  logo: string;
+  n?: boolean;
+}
+
+const Navbar: FC<Props> = ({ children, logo, n }) => {
+  
   //transicion de estilos de la navbar
-  useEffect(() => {   
+  useEffect(() => {
     window?.addEventListener("scroll", changeNavbarColor);
   }, []);
 
@@ -18,27 +27,23 @@ const Navbar = () => {
       className="flex items-center justify-center transition-all duration-300 w-[100%] min-h-[4rem] sm:min-h-[4rem] h-[5rem] z-50 fixed top-0 left-0 right-0"
     >
       <div className="flex justify-between items-center w-[90%]">
+        <Link href={"/goldenelf"}>
           <Image
-            src={"/logo_gonden_elf.png"}
-            alt="Golden elf icon"
+            src={logo}
+            alt="icon"
             height={52.5}
             width={132}
           />
+        </Link>
 
-        <HiOutlineDotsVertical className="flex sm:hidden m-[1rem] text-[1.4rem] text-[#b4b4b7]"/>
+        <HiOutlineDotsVertical className="flex sm:hidden m-[1rem] text-[1.4rem] text-[#b4b4b7]" />
 
         <ul className="justify-between font-light w-[65%] text-[0.75rem] sm:w-[30rem] sm:text-[0.9rem] hidden sm:flex">
-          <li>White Paper</li>
-          <li>Nosotros</li>
-          <Link href={"/ngold"}>
-          <li>Ngold</li>
-          </Link>
-          <li>Tradea</li>
-          <li>Coleccion</li>
+          {children}
         </ul>
 
         <div className="hidden sm:flex ">
-          <WalletButton />
+          <WalletButton n={n} />
         </div>
       </div>
     </nav>
