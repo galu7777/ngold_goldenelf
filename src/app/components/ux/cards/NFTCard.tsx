35c01b3b13e8325e0a5eeac2510deb5c168de79a
Bach16 <gwyn56717@gmail.com>
2024-01-28 22:39:38 -0500

    fix cursor bug

--- src/app/components/ux/cards/NFTCard.tsx
+++ src/app/components/ux/cards/NFTCard.tsx
@@ -50,7 +50,7 @@ const NFTCard: FC<Props> = ({ styles, imgstyles, small }) => {
   };
   
   return (
-    <div onClick={small === true ? handleBuyNFT : ()=>{}} className={` px-[1.4rem] py-[1.4rem] rounded-[1rem] flex flex-col items-center justify-between shadow-sm  bg-white/[0.13] backdrop-blur-sm backdrop-brightness-75 ${small ? " mb-[1rem] sm:w-[15.5rem] h-[23rem]" : "sm:w-[22rem] sm:h-[30rem] w-[17rem] h-[25rem] border-solid border-[1px] border-white/[0.3]"}`}>
+    <div onClick={small === true ? handleBuyNFT : ()=>{}} className={` cursor-pointer px-[1.4rem] py-[1.4rem] rounded-[1rem] flex flex-col items-center justify-between shadow-sm  bg-white/[0.13] backdrop-blur-sm backdrop-brightness-75 ${small ? " mb-[1rem] sm:w-[15.5rem] h-[23rem]" : "sm:w-[22rem] sm:h-[30rem] w-[17rem] h-[25rem] border-solid border-[1px] border-white/[0.3]"}`}>
       <Image
         src={"/7643d9a8-86ca-48eb-be3c-c3f483e71218.jpg"}
         alt="elf_8"
